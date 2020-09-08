cff737f340b50011201d01a4675ec681c8f8ae56
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-09-08 11:27:05 +1000

    Started creating AuthScreen.js for singup/login page

---
+++ components/AuthScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { useNavigatino } from '@react-navigation/native'
+
+export const AuthScreen = ( props ) => {
+
+}
