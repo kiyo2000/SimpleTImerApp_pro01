a9dcc7ffdfdfa2a5f95c55b85e3c4d739d67316c
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-09-10 15:19:38 +1000

    Created a style as viewText

--- components/TimerScreen.js
+++ components/TimerScreen.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { StyleSheet, Text, View  } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View  } from 'react-native';
 
 
 export const TimerScreen = (props) => {
     return (
-        <View>
-            <Text style={styles.title}> - Timer Page - </Text>
-        </View>
+        <SafeAreaView>
+            <View style={styles.viewTitle}>
+                <Text style={styles.title}> - Timer Page - </Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    viewTitle:{
+        height: 30, 
+        width:'100%', 
+        backgroundColor: '#f59842'
+    },
+
     title: {
       textAlign: 'center',
       fontWeight: 'bold',
