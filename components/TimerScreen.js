577243ca046f08fac390a83fe276e31c240b8545
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-09-10 15:42:06 +1000

    Added TouchableOpacity to start text

--- components/TimerScreen.js
+++ components/TimerScreen.js
@@ -1,27 +1,44 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet, Text, View  } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 export const TimerScreen = (props) => {
     return (
-        
+        <View style={styles.mainView}>
             <View style={styles.viewTitle}>
-                <Text style={styles.title}> - Timer Page - </Text>
+                <Text> - Timer Page - </Text>
             </View>
-        
+            <View style={styles.viewButton}>
+                <TouchableOpacity>
+                    <Text> - Press Button - </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    mainView:{
+        flex: 1,
+    },
+
     viewTitle:{
         height: 30, 
         width:'100%', 
-        backgroundColor: '#f59842'
+        backgroundColor: '#f59842',
+        flex: 1,
     },
 
     title: {
-      textAlign: 'center',
-      fontWeight: 'bold', 
-      paddingVertical: 5,
+
     },
+
+    viewButton:{
+        flex: 2,
+    },
+    viewButtonText:{
+
+    },
+
   });
