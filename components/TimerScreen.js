9de3000d1f0310bdedccbda9b7304ef9427214a3
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-09-29 13:05:43 +1000

    The program is back again.

--- components/TimerScreen.js
+++ components/TimerScreen.js
@@ -84,68 +84,68 @@ export const TimerScreen = (props) => {
       fontSize:15,
     },
 });
-/** 
- * Originial codes below -----------------------------------------------------------------------------------------
-*/
-    //Set the initial time to 0 with using useState
-    const [ time, setTime ] = useState( 0 )
-    //Pause timer
-    const [ paused, pauseTime ] = useState( false )
-    // global reference for timer
-    let timer = null
+// /** 
+//  * Originial codes below -----------------------------------------------------------------------------------------
+// */
+//     //Set the initial time to 0 with using useState
+//     const [ time, setTime ] = useState( 0 )
+//     //Pause timer
+//     const [ paused, pauseTime ] = useState( false )
+//     // global reference for timer
+//     let timer = null
 
-    //Display start button text and start timer
-    const startButton = () => {
-        return(
-            <TouchableOpacity
-                style={styles.startButton}
-                onPress={() => { 
-                    //console.log('Start button pressed')//for debugging
-                    setInterval( () => {
-                        if(!paused){
-                        //Display a value 0 and start counting up every thousand second.
-                        setTime(time => time + 1); 
-                        console.log('setInterval() is working');//for debugging
-                        }   
-                    }, 1000);
-                }}>
-                    <Text style={styles.startButtonText}>START</Text>
-            </TouchableOpacity>
-        );
-    }
-    // Display running timmer
-    const timerRunning = () => {
-        return(
-            <TouchableOpacity
-                style={styles.startButton}
-                onPress={() => { 
-                    console.log('button pressed')//for debugging
-                    //Below code still won't stop the timer
-                    pauseTime( { paused: !paused, }); //  paused = true 
-                    console.log('the value of paused is: ', paused ); //for debugging
-                    return () => clearInterval(timer)
-                }}>
-                    <Text style={styles.startButtonText}>{time}</Text>
-            </TouchableOpacity>
-        );
-    }
+//     //Display start button text and start timer
+//     const startButton = () => {
+//         return(
+//             <TouchableOpacity
+//                 style={styles.startButton}
+//                 onPress={() => { 
+//                     //console.log('Start button pressed')//for debugging
+//                     setInterval( () => {
+//                         if(!paused){
+//                         //Display a value 0 and start counting up every thousand second.
+//                         setTime(time => time + 1); 
+//                         console.log('setInterval() is working');//for debugging
+//                         }   
+//                     }, 1000);
+//                 }}>
+//                     <Text style={styles.startButtonText}>START</Text>
+//             </TouchableOpacity>
+//         );
+//     }
+//     // Display running timmer
+//     const timerRunning = () => {
+//         return(
+//             <TouchableOpacity
+//                 style={styles.startButton}
+//                 onPress={() => { 
+//                     console.log('button pressed')//for debugging
+//                     //Below code still won't stop the timer
+//                     pauseTime( { paused: !paused, }); //  paused = true 
+//                     console.log('the value of paused is: ', paused ); //for debugging
+//                     return () => clearInterval(timer)
+//                 }}>
+//                     <Text style={styles.startButtonText}>{time}</Text>
+//             </TouchableOpacity>
+//         );
+//     }
 
-    //Display title text and start button
-    return (
+//     //Display title text and start button
+//     return (
         
-        <View style={styles.mainView}>
-            <View style={styles.viewTitle}>
-                <Text style={styles.titleText}>Let's Start!</Text>
-            </View>
+//         <View style={styles.mainView}>
+//             <View style={styles.viewTitle}>
+//                 <Text style={styles.titleText}>Let's Start!</Text>
+//             </View>
 
-            <View style={styles.viewButton}>
-                {/* Show the running timer if the value of time is greater than 0 */}
-                {time > 0 ? timerRunning() : startButton() }           
-            </View>
-        </View>
-    )
-}
-End of original code -------------------------------------------------------------------------------------------
+//             <View style={styles.viewButton}>
+//                 {/* Show the running timer if the value of time is greater than 0 */}
+//                 {time > 0 ? timerRunning() : startButton() }           
+//             </View>
+//         </View>
+//     )
+// }
+// End of original code -------------------------------------------------------------------------------------------
 
 
 /**
