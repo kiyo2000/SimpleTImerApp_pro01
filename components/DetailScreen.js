95de2f5cf111002fcb6a9793e34b38f982b70a66
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-09-30 13:49:27 +1000

    Display details of each data on DetailsScreen. / The key is  passing props via "route".

--- components/DetailScreen.js
+++ components/DetailScreen.js
@@ -5,7 +5,11 @@ export const DetailScreen = ( props ) =>{
     return(
         <View>
             <Text>Detail page</Text>
+            {/* Display id via route */}
+            <Text>ID: {props.route.params.id}</Text>
+            <Text>Amount: {props.route.params.amount}</Text>
+            <Text>Category: {props.route.params.category}</Text>
+            <Text>Note: {props.route.params.note}</Text>
         </View>
     )
-
 }
