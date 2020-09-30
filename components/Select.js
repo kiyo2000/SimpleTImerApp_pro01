c23e1459806458b9cc12248ef63d2e2d88433f69
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-09-30 23:24:10 +1000

    Created useState() for selecting a category.

--- components/Select.js
+++ components/Select.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Modal, Image } from 'react-native';
 
 export const Select = (props) =>{
-    
+    const [selected, setSelected] = useState('Select category')
+    //Return View with a button
+    return(
+        <View>
+            <TouchableOpacity>
+                <Text>{selected}</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
