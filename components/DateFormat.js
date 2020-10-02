e10ec9082400772a28234e09897d1b5e8f6ae3da
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-10-02 10:00:23 +1000

    Created DateFormat.js

---
+++ components/DateFormat.js
@@ -0,0 +1,22 @@
+import React, {useState,useEffect} from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+export const DateFormat = (props) => {
+  const [date,setDate] = useState('')
+
+  useEffect( () => {
+    const dateObj = new Date( parseInt(props.date) )
+    const date = dateObj.getDate()
+    const day = days[dateObj.getDay()]
+    const month = months[dateObj.getMonth() ]
+    const year = dateObj.getFullYear()
+    setDate(`${day}, ${date} ${month} ${year}`)
+  })
+
+  return (
+    <Text style={{...props.styling}}>{date}</Text>
+  )
+} 
