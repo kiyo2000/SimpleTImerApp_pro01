9caa0a7360a61a0d192c24884ccf9077f5b3a6f8
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-10-03 01:41:45 +1000

    Added " time: item.time " to updateData() on App.js

--- App.js
+++ App.js
@@ -90,7 +90,7 @@ export default function App() {
   }
 
   const updateData = (item) => {
-    const data = {amount: item.amount,note: item.note, category: item.category }
+    const data = {amount: item.amount, note: item.note, category: item.category, time: item.time }
     firebase.database().ref(`${dataRef}/items/${item.id}`).update( data )
     .then(() => {
     })
